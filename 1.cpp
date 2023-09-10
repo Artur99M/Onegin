b42cea63e4482f686dd39ef6b111ba18911b3df2
Artur99M <142691984+Artur99M@users.noreply.github.com>
2023-09-10 20:45:01 +0300

    Delete 1.cpp

--- 1.cpp
+++
@@ -1,7 +0,0 @@
-echo "# Onegin" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Artur99M/Onegin.git
-git push -u origin main
