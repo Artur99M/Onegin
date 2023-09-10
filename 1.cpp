532c034d45c6048c28808e2851a0e851de40520d
Artur99M <142691984+Artur99M@users.noreply.github.com>
2023-09-10 20:40:22 +0300

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,7 @@
+echo "# Onegin" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Artur99M/Onegin.git
+git push -u origin main
