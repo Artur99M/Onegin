de56e1c45eba7c656898dabeabff29afd7569f87
Artur99M <142691984+Artur99M@users.noreply.github.com>
2023-11-03 14:11:34 +0300

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -3,13 +3,14 @@
 #include <math.h>
 #include <assert.h>
 #include <string.h>
+#include <time.h>
 #include <ctype.h>
 
 const int MORE = 1;
 const int EQUAL = 0;
 const int LESS = -1;
 
-int mystrcmp(const line line1, const line line2) {
+int mystrcmp(line line1, line line2) {
 
     for (int i = 0, j = 0; line1.str[i] != '\0' && line2.str[j] != '\0'; i++, j++) {
 
@@ -26,7 +27,7 @@ int mystrcmp(const line line1, const line line2) {
 void sorttext(text* text) {
 
     assert(text != nullptr);
-    sort(text->line, text->numlines, mystrcmp);
+    Qsort(text->line, text->numlines, mystrcmp);
 }
 
 void sort(line* lines, size_t size, int(*cmp)(line, line)) {
@@ -48,7 +49,7 @@ void sort(line* lines, size_t size, int(*cmp)(line, line)) {
 void sortreverse(text* text){
 
     assert(text != nullptr);
-    sort(text->line, text->numlines, strcmprev);
+    Qsort(text->line, text->numlines, strcmprev);
 
  }
 
@@ -65,4 +66,51 @@ int strcmprev(const line line1, const line line2) {
         return (line1.str[i] - line2.str[j]) / abs (line1.str[i] - line2.str[j]);
     }
     return EQUAL;
+}
+void Qsort (line* data, size_t size, int (*cmp)(line, line))
+{
+    assert (data != nullptr);
+    assert (cmp != nullptr);
+
+
+    if (size == 2)
+    {
+        if (cmp(data[0], data[1]) > 0)
+            swap (&data[0], &data[1]);
+        return;
+    }
+
+    if (size == 2)
+    {
+        if (cmp (data[0], data[1]) > 0)
+            swap (&data[0], &data[1]);
+        return;
+    }
+
+    line pivot = data[time(NULL) % size];
+    size_t i = 0, j = size - 1;
+    while (i < j)
+    {
+        if (cmp (data[i], pivot) >= 0 && cmp (data[j], pivot) <= 0)
+            swap (&data[i], &data[j]);
+
+        if (cmp (data[i], pivot) <= 0) i++;
+        if (cmp (data[j], pivot) >= 0) j--;
+
+    }
+
+    if (i > 1)
+        Qsort (data, i, cmp);
+    if (size - i > 1)
+        Qsort (data + i, size - i, cmp);
+}
+
+void swap (line* a, line* b)
+{
+    assert (a != nullptr);
+    assert (b != nullptr);
+
+    line change = *a;
+    *a = *b;
+    *b = change;
 }
